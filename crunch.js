339376a356ce89c8dafd40c881c428e2e76a497e
Adam Lofting <contactme@adamlofting.com>
2014-10-14 22:26:25 +0100

    building some more queries

--- crunch.js
+++ crunch.js
@@ -1,23 +1,23 @@
 var webmakerMetrics = require('./lib/webmaker-metrics.js');
 var appmaker = require('./lib/appmaker_temp.js');
 
-// webmakerMetrics.checkAndCrunch(function (err, res) {
-//   'use strict';
-//   if (err) {
-//     console.error(err);
-//     process.exit(0);
-//   }
-//   console.log(res);
-//   //process.exit(0);
-// });
-
-webmakerMetrics.updateEngagementMetrics(function (err, res) {
+webmakerMetrics.updateRIDMetrics(function (err, res) {
   'use strict';
   if (err) {
     console.error(err);
     process.exit(0);
   }
+  console.log(res);
+  //process.exit(0);
 });
+
+// webmakerMetrics.updateEngagementMetrics(function (err, res) {
+//   'use strict';
+//   if (err) {
+//     console.error(err);
+//     process.exit(0);
+//   }
+// });
 
 // UPDATE APPMAKER
 // appmaker.refreshStats(function (err) {
